0ded1d03a638adbab0951494d1f343143f0681cb
dzidungs <dzidungs@gmail.com>
2022-04-07 13:23:26 +0200

    fix columns bug

--- views/site/welcome.php
+++ views/site/welcome.php
@@ -40,7 +40,7 @@ WelcomeAsset::register($this);
         <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
             <div class="container px-5">
                 <a class="navbar-brand fw-bold" href="#page-top">
-                        <div class="sidebar-brand-text mx-3">
+                        <div class="sidebar-brand-text mx-3 rotate-n-15">
                             <i class="fab fa-artstation" style="font-size: 1.5rem"></i> Invest Tracker</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
